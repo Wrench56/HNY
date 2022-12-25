2fd48bc30279f5d0002d4b7ab8e3f323ce968e27
Wrench56 <dmarkreg@gmail.com>
2022-12-25 23:15:57 +0100

    Added basic animation (year string shifting)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate pancurses;
 
 use pancurses::{initscr, endwin, Window};
-use std::{env, process::exit};
+use std::{env, process::exit, thread};
+
+use unicode_segmentation::UnicodeSegmentation;
+
 
 mod art;
 
@@ -17,12 +20,33 @@ fn main() {
 
     let stdscr: Window = initscr();
     stdscr.printw("Hello");
-    prev_to_new(&stdscr, &new_year)
+    prev_to_new(&stdscr, &new_year);
     stdscr.refresh();
     stdscr.getch();
     endwin();
 }
 
 fn prev_to_new(_stdscr: &Window, _new_year: &u32) {
-    art::str_to_art(&(_new_year-1).to_string());
+    const padding: i32 = 3;
+
+    let x: i32 = _stdscr.get_max_x();
+    let y: i32 = _stdscr.get_max_y();
+
+    let prev_year: Vec<String> = art::str_to_art(&(_new_year-1).to_string());
+    let curr_year: Vec<String> = art::str_to_art(&_new_year.to_string());
+
+    let year_x_pos: i32 = (((x-(prev_year[0].graphemes(true).count() as i32))/2) as f32).floor() as i32;
+
+    let mut year_y_pos: i32 = 0;
+    for i in 0..(y-(prev_year.len() as i32)-padding) {
+        _stdscr.erase();
+        year_y_pos = i;
+        for line in &prev_year {
+            _stdscr.mv(year_y_pos+padding, year_x_pos);
+            _stdscr.addstr(line);
+            year_y_pos += 1;
+        }
+        _stdscr.refresh();       
+        thread::sleep_ms(100);
+    }
 }
