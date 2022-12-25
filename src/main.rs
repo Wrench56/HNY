50971e4ffb214a20e92ed5992c01f1fea1bae49a
Wrench56 <dmarkreg@gmail.com>
2022-12-25 22:08:57 +0100

    Added pancurses (basic print on screen)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate pancurses;
+
+use pancurses::{initscr, endwin, Window};
+use std::{env, process::exit};
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: HNY <year>\nArguments:\n - year: provide the new year");
+        exit(0)
+    }
+
+    let new_year: &u32 = &args[1].parse::<u32>().unwrap();
+
+
+    let stdscr: Window = initscr();
+    stdscr.printw("Hello");
+    stdscr.refresh();
+    stdscr.getch();
+    endwin();
 }
