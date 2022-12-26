4c8f62818a050dc07baef387c9b242841ca72900
Wrench56 <dmarkreg@gmail.com>
2022-12-26 18:19:39 +0100

    Added Firework::new() method

--- src/firework.rs
+++ src/firework.rs
@@ -1,3 +1,7 @@
+extern crate pancurses;
+
+use pancurses::Window;
+
 enum FireworkState {
     Spawn,
     Flying,
@@ -14,7 +18,16 @@ pub struct Firework {
 }
 
 impl Firework {
-    fn next_cycle(&mut self) {
+    pub fn new(x_pos: i32, y_pos: i32, release_height: i32, color: i16) -> Firework {
+        Firework{
+            x_pos: x_pos,
+            y_pos: y_pos,
+            release_height: release_height,
+            color: color,
+            state: FireworkState::Spawn
+        }
+    }
+    pub fn next_cycle(&mut self, _stdscr: &Window) {
         match self.state {
             FireworkState::Spawn => {
                 self.state = FireworkState::Flying;
