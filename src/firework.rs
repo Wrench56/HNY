05754e69db6d3fe4d9ec07cb8320d686c3550cf0
Wrench56 <dmarkreg@gmail.com>
2022-12-26 21:44:24 +0100

    Added firework animation & 3rd Explosion "frame"

--- src/firework.rs
+++ src/firework.rs
@@ -1,12 +1,13 @@
 extern crate pancurses;
 
-use pancurses::Window;
+use pancurses::{Window, COLOR_PAIR};
 
 enum FireworkState {
     Spawn,
     Flying,
     Explosion1,
     Explosion2,
+    Explosion3,
     Dead
 }
 
@@ -15,12 +16,12 @@ pub struct Firework {
     x_pos: i32,
     y_pos: i32,
     release_height: i32,
-    color: i16,
+    color: u64,
     state: FireworkState,
 }
 
 impl Firework {
-    pub fn new(x_pos: i32, y_pos: i32, release_height: i32, color: i16) -> Firework {
+    pub fn new(x_pos: i32, y_pos: i32, release_height: i32, color: u64) -> Firework {
         Firework{
             x_pos: x_pos,
             y_pos: y_pos,
@@ -29,21 +30,62 @@ impl Firework {
             state: FireworkState::Spawn
         }
     }
-    pub fn next_cycle(&mut self, _stdscr: &Window) -> bool{
+    pub fn next_cycle(&mut self, stdscr: &Window) -> bool{
         match self.state {
             FireworkState::Spawn => {
+                stdscr.attron(COLOR_PAIR(7));
+                stdscr.mv(self.y_pos, self.x_pos);
+                stdscr.addch('|');
+                stdscr.mv(self.y_pos-1, self.x_pos);
+                stdscr.addch('.');
+
+                self.y_pos -= 1;
+
                 self.state = FireworkState::Flying;
                 true
             }
             FireworkState::Flying => {
-                self.state = FireworkState::Explosion1;
+                stdscr.attron(COLOR_PAIR(7));
+                stdscr.mv(self.y_pos, self.x_pos);
+                stdscr.addch('|');
+                stdscr.mv(self.y_pos-1, self.x_pos);
+                stdscr.addch('.');
+                
+                self.y_pos -= 1;
+
+                if self.y_pos <= self.release_height {
+                    self.state = FireworkState::Explosion1;
+                }
                 true
             }
             FireworkState::Explosion1 => {
+                stdscr.attron(COLOR_PAIR(self.color));
+                stdscr.mvprintw(self.y_pos-1, self.x_pos-1, "* *");
+                stdscr.mvprintw(self.y_pos, self.x_pos-2, "* # *");
+                stdscr.mvprintw(self.y_pos+1, self.x_pos-1, "* *");
+
                 self.state = FireworkState::Explosion2;
                 true
             }
             FireworkState::Explosion2 => {
+                stdscr.attron(COLOR_PAIR(self.color));
+                stdscr.mvprintw(self.y_pos-2, self.x_pos-1, "*''*");
+                stdscr.mvprintw(self.y_pos-1, self.x_pos-2, "*_\\/_*");
+                stdscr.mvprintw(self.y_pos, self.x_pos-2, "* /\\ *");
+                stdscr.mvprintw(self.y_pos+1, self.x_pos-1, "*''*");
+
+                self.state = FireworkState::Explosion3;
+                true
+            }
+            FireworkState::Explosion3 => {
+                stdscr.attron(COLOR_PAIR(self.color));
+                stdscr.mvprintw(self.y_pos-3, self.x_pos, "..");
+                stdscr.mvprintw(self.y_pos-2, self.x_pos-2, "*\\::/*");
+                stdscr.mvprintw(self.y_pos-1, self.x_pos-3, "*__\\/__*");
+                stdscr.mvprintw(self.y_pos, self.x_pos-3, "*  /\\  *");
+                stdscr.mvprintw(self.y_pos+1, self.x_pos-2, "*/::\\*");
+                stdscr.mvprintw(self.y_pos+2, self.x_pos, "ˇˇ");
+
                 self.state = FireworkState::Dead;
                 true
             }
