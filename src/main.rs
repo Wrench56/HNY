1a0a5caeb10734a72dc8be84590c4c0fbe204486
Wrench56 <dmarkreg@gmail.com>
2022-12-26 20:36:42 +0100

    Added firework loop (main loop)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate pancurses;
 extern crate rand;
 
-use pancurses::{initscr, endwin, Window, Input};
+use pancurses::{initscr, endwin, Window};
 use std::{env, process::exit, thread, time};
 use rand::Rng;
 
@@ -9,6 +9,9 @@ use unicode_segmentation::UnicodeSegmentation;
 use console::Term;
 
 mod art;
+mod color;
+
+mod firework;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -21,6 +24,9 @@ fn main() {
 
     // Initialize ncurses window
     let stdscr: Window = initscr();
+
+    // Initialize ncurses colors
+    color::init_colors();
 
     // Create exit thread
     thread::spawn(|| {
@@ -73,9 +79,29 @@ fn prev_to_new(_stdscr: &Window, _new_year: &u32) {
 }
 
 fn firework_loop(_stdscr: &Window) {
+    let x: i32 = _stdscr.get_max_x();
+    let y: i32 = _stdscr.get_max_y();
+
+    let mut rng = rand::thread_rng();
+
+    let mut fireworks: Vec<firework::Firework> = Vec::new();
     loop {
-        
-        
+        for _ in 0..rng.gen_range(0..3) {
+            fireworks.push(firework::Firework::new(
+                rng.gen_range(5..(x-5)),
+                0,
+                rng.gen_range(((y/2) as f32).floor() as i32..((y/3) as f32*2 as f32).floor() as i32),
+                rng.gen_range(1..7),
+            ));
+        }
+        let mut item_index: i16 = fireworks.len() as i16 - 1 ; // i8 might be sufficient
+        while item_index > 0 {
+            if fireworks[item_index as usize].next_cycle(_stdscr) == false {
+                fireworks.remove(item_index as usize);
+            }
+            item_index -= 1;
+        }
+        _stdscr.refresh();
     }
 }
 
