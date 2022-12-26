1119aab2591f673fd74ffbdd8a1e1cdf2799a0f0
Wrench56 <dmarkreg@gmail.com>
2022-12-26 18:18:15 +0100

    Removed return (instead using the prefered idiomatic expression)

--- src/art.rs
+++ src/art.rs
@@ -3,9 +3,9 @@ extern crate cfonts;
 use cfonts::{render, Options, Fonts};
 
 pub fn str_to_art(string: &String) -> Vec<String>{
-    return render(Options {
+    render(Options {
 		text: String::from(string),
 		font: Fonts::FontHuge,
 		..Options::default()
-    }).text.lines().filter(|x| *x != "").map(str::to_string).collect();
+    }).text.lines().filter(|x| *x != "").map(str::to_string).collect()
 }
