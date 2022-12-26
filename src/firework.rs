68ec7c384723851d86685924877d4f2a87024bb1
Wrench56 <dmarkreg@gmail.com>
2022-12-26 04:49:18 +0100

    Added instance variables/fields & repaired next_cycle() in the Firework impl

--- src/firework.rs
+++ src/firework.rs
@@ -5,14 +5,20 @@ enum FireworkState {
 }
 
 
-struct Firework {
+pub struct Firework {
+    x_pos: i32,
+    y_pos: i32,
+    release_height: i32,
+    color: i16,
     state: FireworkState,
 }
 
 impl Firework {
-    fn next_cycle(&self) {
+    fn next_cycle(&mut self) {
         match self.state {
-            FireworkState::Spawn =>{}
+            FireworkState::Spawn => {
+                self.state = FireworkState::Flying;
+            }
             FireworkState::Flying => {}
             FireworkState::Explosion => {}
         }
