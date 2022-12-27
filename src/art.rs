c55be659b928f2c211a0e34e1f64cc59dc4e32e6
Wrench56 <dmarkreg@gmail.com>
2022-12-27 18:37:48 +0100

    Added ASCII text drawing function

--- src/art.rs
+++ src/art.rs
@@ -1,5 +1,7 @@
 extern crate cfonts;
+extern crate pancurses;
 
+use pancurses::Window;
 use cfonts::{render, Options, Fonts};
 
 pub fn str_to_art(string: &String) -> Vec<String>{
@@ -8,4 +10,12 @@ pub fn str_to_art(string: &String) -> Vec<String>{
 		font: Fonts::FontHuge,
 		..Options::default()
     }).text.lines().filter(|x| *x != "").map(str::to_string).collect()
+}
+
+pub fn draw_ascii_text(stdscr: &Window, ascii_text: &Vec<String>, y: &mut i32, x: &i32) {
+	for line in ascii_text {
+		stdscr.mv(*y, *x);
+		stdscr.addstr(line);
+		*y += 1;
+	}
 }
